192077f5a7a05ada50fe159e214f1dfc2a29f861
HARSHHARSANI <harshharsani007@gmail.com>
2023-12-01 11:25:07 +0530

    productModel

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,5 +1,82 @@
 import mongoose from "mongoose";
+import { ObjectId } from mongoose.Schema;
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32,
+        text : true 
+    },
+    slug: {
+        type: String,
+        unique : true,
+        lowercase: true,
+        index : true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 1000,
+        text : true 
+    }, 
+    price: {
+        type: Number,
+        required: true,
+        trim: true,
+        maxlength : 32, 
+    }, 
+
+    category: {
+        type: ObjectId,
+        ref: "Category",
+    },
+
+    subCategory: [
+        {
+        type: ObjectId,
+        ref: "SubCategory",
+        },
+    ],
+    
+    quantity: Number,
+
+    sold: {
+        type: Number,
+        default : 0,
+    },
+    
+    images: {
+        type :Array
+    },
+
+    Shipping: {
+        type: String,
+        enum: ["Yes", "No"],
+    },
+
+    color: {
+        type: String,
+        enum: ["Black", "Brown", "White", "Silver", "Blue"],
+    },
+    brand: {
+        type: String,
+        enum: ["Apple", "Microsoft", "Lenovo", "Asus", "Dell" , "Hp" , "Jio" , "Samsung"],
+    },
+
+    // rating: [
+    //     {
+    //         star: Number,
+    //         PostedBy: {
+    //             type: ObjectId,
+    //             ref:"Users",
+    //         }
+    //     }
+    // ]
+
+},
+    { timestamps: true }
+);
 
 export default mongoose.model("products", productSchema);

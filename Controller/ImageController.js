351fe767d1d4be07788fd0d71c2ced635befbb7b
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-03-19 22:18:09 +0530

    test3@adi

--- Controller/ImageController.js
+++ Controller/ImageController.js
@@ -3,8 +3,8 @@ export const allimagesController = async (req, res) => {
     const response = {
       "123456789": {
         "imageURL":
-          "https://drive.google.com/file/d/13Wm3jwcDsXHqMQLm1JYJ_IB5MUvrX-8_/view?usp=sharing",
-        "count": "5",
+          "https://firebasestorage.googleapis.com/v0/b/netflix-fbca8.appspot.com/o/youtube%20ad.png?alt=media&token=e341bf44-fc3d-483c-bd76-5925d4a7697d",
+        "count": 5,
       },
     };
     res.status(200).json(response);

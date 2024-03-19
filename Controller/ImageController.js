0fd9cb98f5aaf9d5ec7471927d6d81d6d32cde71
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-03-19 21:58:50 +0530

    test2adi

--- Controller/ImageController.js
+++ Controller/ImageController.js
@@ -1,10 +1,10 @@
 export const allimagesController = async (req, res) => {
   try {
     const response = {
-      123456789: {
-        ImageURL:
+      "123456789": {
+        "imageURL":
           "https://drive.google.com/file/d/13Wm3jwcDsXHqMQLm1JYJ_IB5MUvrX-8_/view?usp=sharing",
-        Count: "5",
+        "count": "5",
       },
     };
     res.status(200).json(response);

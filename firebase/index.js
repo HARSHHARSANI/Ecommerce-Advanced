179b3cc4979a932050df69439798482bf80cea0a
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-05-06 13:31:33 +0530

    code

--- firebase/index.js
+++ firebase/index.js
@@ -1,5 +1,5 @@
 // firebase/index.js
-import admin from "firebase-admin";
+import admin from "../node_modules/firebase-admin";
 import serviceAccount from "../config/fbServiceAccountKey.json" assert { type: "json" };
 
 admin.initializeApp({
